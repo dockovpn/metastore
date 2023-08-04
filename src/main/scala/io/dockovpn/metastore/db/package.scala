4d8d9bf3ca21341d843edb530dcf0c05d60419bc
Alexander Litvinenko <alekslitvinenk@gmail.com>
2023-08-04 15:13:08 +0300

    Add noopDB DBRef

--- src/main/scala/io/dockovpn/metastore/db/package.scala
+++ src/main/scala/io/dockovpn/metastore/db/package.scala
@@ -5,8 +5,12 @@
 package io.dockovpn.metastore
 
 import io.dockovpn.metastore.util.Lazy
+import io.dockovpn.metastore.util.Lazy.lazily
 import slick.jdbc.MySQLProfile
+import slick.jdbc.MySQLProfile.api._
 
 package object db {
   type DBRef = Lazy[MySQLProfile.backend.Database]
+  
+  def noopDB: DBRef = lazily { Database.forConfig("") }
 }
